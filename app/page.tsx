296924f5937280bf2da9cc5843a2d85e6b9d61e0
Hilal Ramadhan <hilalramadhan@Hilals-MacBook-Pro.local>
2025-06-29 23:26:45 +0700

    hardcode token

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
   // but this can be easily replaced with a login layer and session management
   // const { token: jwtToken } = useAccessToken();
   const jwtToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBNTcyNTg0MjY3ODk2MS1TT0ZUV0FSRU9ORV9QQVJUTkVSLkhJTEFMX1IuU0hBMjU2OlZvNkhIZ3crLzMxVTM3WCtEUHJHK1BYZTIzWlRocUZxOWR4K1lyb0RzWXM9Iiwic3ViIjoiQTU3MjU4NDI2Nzg5NjEtU09GVFdBUkVPTkVfUEFSVE5FUi5ISUxBTF9SIiwiaWF0IjoxNzUxMjA4OTUwLCJleHAiOjE3NTEyMTI1NTB9.ScBd83lyCbYGq_EORTbAYiYLUpE1DQoxN1av_QUta1BZZUq1eAzS8gP77BZ_GZePxaFhJjB6-7lbDK4pO0-B08BzVjNqSJrcN2-xZc_9sq9gEh5HWPw3j6t7Kl-ZcIxNLxBUiW9dyWfLtIxYxt4t5j2fxVZSDZ4lMdh-sDQP7d3FYXCN19FQqsn4MUyyts5-xmkAyxHGOT39zesmWcQL5XZvwetDlb9GJyCNObK_tMY8tFIeluMQl5M5On5xdVwBhZDxK_t_E-Xb3NMCHPwFlhluIrYfsM7VCsuhroXpuV4Sa0nVO07Juld2WjGnReZT00MF7GYkTovkmHcI2oPwTQ";
-  
 
   const tools: AgentRequestBuildParams['tools'] = [
     CORTEX_SEARCH_TOOL,
