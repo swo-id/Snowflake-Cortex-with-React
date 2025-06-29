31e1006e8733d5ec47d251694ba76caae1549792
Hilal Ramadhan <hilalramadhan@Hilals-MacBook-Pro.local>
2025-06-29 22:57:27 +0700

    update netlify

--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,15 @@ import { Messages } from "./components/messages";
 import { ChatInput } from "./components/input";
 import { ChatHeader } from "./components/chat-header";
 
-import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
+// import { useEffect, useState } from "react";
+// import { useSearchParams } from "next/navigation";
 
 export default function Home() {
   // Agent API requires a JWT auth token. For simplicity we are using an api to fetch this,
   // but this can be easily replaced with a login layer and session management
   // const { token: jwtToken } = useAccessToken();
-  // dari sini
-  const searchParams = useSearchParams();
-  const tokenFromURL = searchParams?.get("token");
-  const auth = useAccessToken();
-
-  const [jwtToken, setJwtToken] = useState<string | undefined>(auth?.token);
-
-  useEffect(() => {
-    if (!auth?.token && tokenFromURL) {
-      setJwtToken(tokenFromURL);
-    }
-  }, [auth?.token, tokenFromURL]);
-  // sampe sini
+  const jwtToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJBNTcyNTg0MjY3ODk2MS1TT0ZUV0FSRU9ORV9QQVJUTkVSLkhJTEFMX1IuU0hBMjU2OlZvNkhIZ3crLzMxVTM3WCtEUHJHK1BYZTIzWlRocUZxOWR4K1lyb0RzWXM9Iiwic3ViIjoiQTU3MjU4NDI2Nzg5NjEtU09GVFdBUkVPTkVfUEFSVE5FUi5ISUxBTF9SIiwiaWF0IjoxNzUxMjA4OTUwLCJleHAiOjE3NTEyMTI1NTB9.ScBd83lyCbYGq_EORTbAYiYLUpE1DQoxN1av_QUta1BZZUq1eAzS8gP77BZ_GZePxaFhJjB6-7lbDK4pO0-B08BzVjNqSJrcN2-xZc_9sq9gEh5HWPw3j6t7Kl-ZcIxNLxBUiW9dyWfLtIxYxt4t5j2fxVZSDZ4lMdh-sDQP7d3FYXCN19FQqsn4MUyyts5-xmkAyxHGOT39zesmWcQL5XZvwetDlb9GJyCNObK_tMY8tFIeluMQl5M5On5xdVwBhZDxK_t_E-Xb3NMCHPwFlhluIrYfsM7VCsuhroXpuV4Sa0nVO07Juld2WjGnReZT00MF7GYkTovkmHcI2oPwTQ";
+  
 
   const tools: AgentRequestBuildParams['tools'] = [
     CORTEX_SEARCH_TOOL,
@@ -35,7 +24,7 @@ export default function Home() {
   ]
 
   const { agentState, messages, latestAssistantMessageId, handleNewMessage } = useAgentAPIQuery({
-    authToken: jwtToken ?? "",
+    authToken: jwtToken,
     snowflakeUrl: process.env.NEXT_PUBLIC_SNOWFLAKE_URL!,
     experimental: {
       EnableRelatedQueries: true,
